0521ed9bbd2a51f865bcff0f9645af915b67b31f
saad-out <outsaad03@gmail.com>
2023-04-17 18:22:28 +0000

    push into new array

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
-  for (const [idx, element] of array.entries()) {
-    newArray[idx] = appendString + element;
+  for (const element of array) {
+    newArray.push(appendString + element);
   }
 
   return newArray;
