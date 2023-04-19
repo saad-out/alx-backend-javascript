2593d8e13ef443042fc83bf6ca01de3ef7247a12
saad-out <outsaad03@gmail.com>
2023-04-19 21:53:44 +0000

    multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+    .then((response) => {
+      const responseArr = [];
+      response.forEach((data) => {
+        responseArr.push({
+          status: 'success',
+          value: data,
+        });
+      });
+    })
+    .catch((error) => {
+      [{
+        "status": "rejected",
+        value: error
+      }]
+    });
+}
