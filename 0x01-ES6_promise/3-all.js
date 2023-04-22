1bd30cb652adeba528f22f6219d232536acd527f
saad-out <outsaad03@gmail.com>
2023-04-22 01:57:59 +0000

    call functions as arguments

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photo = uploadPhoto();
-  const user = createUser();
-
-  Promise.all([photo, user])
+  Promise.all([uploadPhoto(), createUser()])
     .then((data) => {
       console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
     })
