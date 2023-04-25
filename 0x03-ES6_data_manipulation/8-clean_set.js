a227efc4bb82f87a3245b8f8657cc770abcc02d3
saad-out <outsaad03@gmail.com>
2023-04-25 10:46:17 +0100

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
+  let returnString = '';
+  const startStringLength = startString.length;
+  set.forEach((string) => {
+    if (string.startsWith(startString)) {
+      if (returnString !== '') {
+        returnString += '-';
+      }
+      returnString += string.slice(startStringLength);
+    }
+  });
+  return returnString;
+}
