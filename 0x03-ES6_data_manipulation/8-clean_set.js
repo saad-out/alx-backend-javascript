96b88c006e6574ae2a93cdcfd2b3cb17f9a7ed48
saad-out <outsaad03@gmail.com>
2023-04-26 10:59:47 +0100

    handle wrong arg type & undefined values

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
-export default function cleanSet(set, startString) {
-  if (startString === '') {
+export default function cleanSet(set, startString = '') {
+  if (typeof startString !== 'string' || startString === '') {
     return '';
   }
 
   let returnString = '';
   const startStringLength = startString.length;
   set.forEach((string) => {
-    if (string.startsWith(startString)) {
+    if (string !== undefined && string.startsWith(startString)) {
       if (returnString !== '') {
         returnString += '-';
       }
