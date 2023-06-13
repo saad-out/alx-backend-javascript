372136c883ae9f2ae343432fb84493714454decc
saad-out <outsaad03@gmail.com>
2023-06-13 10:56:53 +0000

    stdin & stdout

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdin.setEncoding("utf8");
+
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.on("readable", function () {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
+});
+
+process.stdin.on("end", function () {
+  process.stdout.write("This important software is now closing\n");
+});
