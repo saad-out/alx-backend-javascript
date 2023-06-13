1debd6e86aefc9ee7c23f7e745d8528189c49f9d
saad-out <outsaad03@gmail.com>
2023-06-13 11:57:41 +0100

    fix eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-process.stdin.setEncoding("utf8");
+process.stdin.setEncoding('utf8');
 
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-process.stdin.on("readable", function () {
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input !== null) {
     process.stdout.write(`Your name is: ${input}`);
   }
 });
 
-process.stdin.on("end", function () {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
